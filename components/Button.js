f7877923012b549d4dff244ee9212e99f86e65b6
Abhaywadkar04 <abhaywadkar2004@gmail.com>
2024-07-29 19:24:15 +0530

    done

---
+++ components/Button.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { motion , useMotionValue } from 'framer-motion'
+
+// function Button() {
+//   return (
+//     // <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+//     // <motion.button
+//     //   className="example-button"
+//     //   style={{ backgroundColor: "red" }}
+//     //   whileHover={{
+//     //     scale: 1.2,
+//     //     rotate: "2.5deg",
+//     //     transition: { duration: 1,ease: "easeInOut" },
+        
+//     //   }}
+      
+//     //   whileTap={{ scale: 1.0 }}
+//     // >
+//     //   Click me!
+//     // </motion.button>
+//     // </div>
+    
+
+
+//     const MyComponent = () => {
+//       const [isHovered, setHovered] = useState(false)
+    
+//       // Simplified example
+//       return (
+//         <svg>
+//           <image
+//             filter="url(#blur)"
+//             onMouseEnter={() => setHovered(true)}
+//             onMouseLeave={() => setHovered(false)}
+//           />
+//           <filter id="blur">
+//             <motion.div
+//               initial={false}
+//               animate={{ stdDeviation: isHovered ? 0 : 2 }}
+//             />
+//           </filter>
+//         </svg>
+//       )
+//     }
+// // )}
+
+// export default Button
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import AnimationControls from "../components/Animationcontrols";
+import BasicsOfMotion from "../components/BasicsOfMotion";
+import Gestures from "../components/Gestures";
+import ScrollAnimations from "../components/ScrollAnimation";
+import ViewBasedAnimations from "../components/ViewBaseAnimaions";
+import Button from "../components/Button";
+export default function Home() {
+  return (
+    <div>
+      {/* <BasicsOfMotion /> */}
+      {/* <Gestures />  */}
+      {/* <Button/> */}
+      {/* {<AnimationControls /> }  */}
+      {<ViewBasedAnimations /> }
+      {/* <ScrollAnimations />  */}
+    </div>
+  );
+}
